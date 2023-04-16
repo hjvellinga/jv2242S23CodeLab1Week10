48e1614e2c1e899ea4b85fcc927dc289f6f47adf
Apolho <jjhern2017@gmail.com>
2023-04-16 15:13:47 -0400

    Pieces are Placed

--- 2D Array Week 10/Assets/Scripts/SimpleCheckersScript.cs
+++ 2D Array Week 10/Assets/Scripts/SimpleCheckersScript.cs
@@ -38,6 +38,86 @@ public class SimpleCheckersScript : MonoBehaviour
                 grid[x, y] = 0; //the grid is empty
             }
         }
+        #region Column0
+        grid[0, 0] = 0; 
+        grid[0, 1] = 1; 
+        grid[0, 2] = 0;
+        grid[0, 3] = 1;
+        grid[0, 4] = 0;
+        grid[0, 5] = 1;
+        grid[0, 6] = 0;
+        grid[0, 7] = 1;
+        #endregion
+        #region Column1
+        grid[1, 0] = 1;
+        grid[1, 1] = 0;
+        grid[1, 2] = 1;
+        grid[1, 3] = 0;
+        grid[1, 4] = 1;
+        grid[1, 5] = 0;
+        grid[1, 6] = 1;
+        grid[1, 7] = 0;
+        #endregion
+        #region Column2
+        grid[2, 0] = 0;
+        grid[2, 1] = 1;
+        grid[2, 2] = 0;
+        grid[2, 3] = 1;
+        grid[2, 4] = 0;
+        grid[2, 5] = 1;
+        grid[2, 6] = 0;
+        grid[2, 7] = 1;
+        #endregion
+        #region Column3
+        grid[3, 0] = 0;
+        grid[3, 1] = 0;
+        grid[3, 2] = 0;
+        grid[3, 3] = 0;
+        grid[3, 4] = 0;
+        grid[3, 5] = 0;
+        grid[3, 6] = 0;
+        grid[3, 7] = 0;
+        #endregion
+        #region Column4
+        grid[4, 0] = 0;
+        grid[4, 1] = 0;
+        grid[4, 2] = 0;
+        grid[4, 3] = 0;
+        grid[4, 4] = 0;
+        grid[4, 5] = 0;
+        grid[4, 6] = 0;
+        grid[4, 7] = 0;
+        #endregion
+        #region Column5
+        grid[5, 0] = 2;
+        grid[5, 1] = 0;
+        grid[5, 2] = 2;
+        grid[5, 3] = 0;
+        grid[5, 4] = 2;
+        grid[5, 5] = 0;
+        grid[5, 6] = 2;
+        grid[5, 7] = 0;
+        #endregion
+        #region Column6
+        grid[6, 0] = 0;
+        grid[6, 1] = 2;
+        grid[6, 2] = 0;
+        grid[6, 3] = 2;
+        grid[6, 4] = 0;
+        grid[6, 5] = 2;
+        grid[6, 6] = 0;
+        grid[6, 7] = 2;
+        #endregion
+        #region Column7
+        grid[7, 0] = 2;
+        grid[7, 1] = 0;
+        grid[7, 2] = 2;
+        grid[7, 3] = 0;
+        grid[7, 4] = 2;
+        grid[7, 5] = 0;
+        grid[7, 6] = 2;
+        grid[7, 7] = 0;
+        #endregion
         UpdateDisplay();
     }
 
